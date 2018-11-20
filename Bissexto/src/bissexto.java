0718d6b5555581156eb790ee893c01c6d68ef99c
andre1126 <andre-filho08@hotmail.com>
2018-11-19 21:17:14 -0300

    Adicionando comentários

--- Bissexto/src/bissexto.java
+++ Bissexto/src/bissexto.java
@@ -1,3 +1,10 @@
+//BISSEXTO - 9 Novembro de 2018 
+
+//A cada 4 anos, a diferença de horas entre o ano solar e o do calendário convencional completa cerca de 24 horas (mais exatamente: 23 horas, 15 minutos e 864 milésimos de segundo). Para compensar essa diferença e evitar um descompasso em relação às estações do ano, insere-se um dia extra no calendário e o mês de fevereiro fica com 29 dias. Essa correção é especialmente importante
+//para atividades atreladas às estações, como a agricultura e até mesmo as festas religiosas. Um determinado ano é bissexto se:
+// 
+//     • O ano for divisível por 4, mas não divisível por 100, exceto se ele for também divisível por 400.
+
 import java.util.Scanner;
 
 public class bissexto {
--- FizzBuzz/src/FizzBuzz/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz/FizzBuzz.java
@@ -1,31 +1,46 @@
+//FIZZBUZZ - 17 de Novembro de 2018 
+
+//Neste problema, você deverá exibir uma lista de 1 a 100, um em cada linha, com as seguintes exceções:
+//    • Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
+//    • Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
+//    • Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
+
 package FizzBuzz;
 
 public class FizzBuzz {
 	public static void main(String[] args) {
+		// Bloco de repetição para contar até 100.
 		for (int i = 1; i <= 100; i++) {
-			/*
-			 * int mod3 = i % 3; int mod5 = i % 5; if (mod3 == 0 && mod5 == 0) {
-			 * System.out.println("FizzBuzz"); } else if (mod3 == 0) {
-			 * System.out.println("Fizz"); } else if (mod5 == 0) {
-			 * System.out.println("Buzz"); } else { System.out.println(i); }
-			 */
-			String mod = "";
-
-			mod = (i % 3 == 0 && i % 5 == 0) ? "35" : (i % 3 == 0) ? "3" : (i % 5 == 0) ? "5" : "";
-
-			switch (mod) {
-			case "3":
-				System.out.println("Fizz");
-				break;
-			case "5":
-				System.out.println("Buzz");
-				break;
-			case "35":
+			
+			// cálculo para checar se o número é divisível por 3 ou po4 5
+			int mod3 = i % 3;
+			int mod5 = i % 5;
+			
+			//Checa se é divisível por 3 e 5 ao mesmo tempo.
+			if (mod3 == 0 && mod5 == 0) {
 				System.out.println("FizzBuzz");
-			default:
+				
+			//Checa se é divisível por 3.
+			} else if (mod3 == 0) {
+				System.out.println("Fizz");
+			
+			//Checa se é divisível por 5.	
+			} else if (mod5 == 0) {
+				System.out.println("Buzz");
+				
+			//Se não for divisível por nada, simplesmente escreve o número.
+			} else {
 				System.out.println(i);
-				break;
 			}
+
+			/* Versão de Alday Pinheiro 
+			 * 
+			 * String mod = ""; mod = (i % 3 == 0 && i % 5 == 0) ?
+			 * "3 e 5" : (i % 3 == 0) ? "3" : (i % 5 == 0) ? "5" : ""; switch (mod) { case
+			 * "3": System.out.println("Fizz"); break; case "5": System.out.println("Buzz");
+			 * break; case "3 e 5": System.out.println("FizzBuzz"); default:
+			 * System.out.println(i); break; }
+			 */
 		}
 	}
 }
